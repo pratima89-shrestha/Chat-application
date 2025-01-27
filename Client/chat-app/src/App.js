061c32622751c0334abf05d1c3f9a5e898bef4bd
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-27 21:40:53 +0545

    a new commit

--- Client/chat-app/src/App.js
+++ Client/chat-app/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'; // Correct import for v6
-import HomePage from './pages/HomePage';//Ensure HomePage is default exported
+import HomePage from './pages/HomePage'; // Ensure HomePage is default exported
 import ChatPage from './pages/ChatPage';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
+      <div className="image-container">
+        {/* Background image is applied through CSS */}
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/chats" element ={<ChatPage />} />
+        <Route path="/chats" element={<ChatPage />} />
       </Routes>
     </div>
   );
--- Client/chat-app/src/pages/HomePage.js
+++ Client/chat-app/src/pages/HomePage.js
@@ -4,9 +4,8 @@ import {Container, Box, Text} from '@chakra-ui/react';
 export const HomePage = () => {
   return (
    <Container maxW='xl' centerContent>
-    <Box
-    >
-      <Text>Guff Ghar</Text>
+    <Box>
+      <Text>GUFF GHAR</Text>
     </Box>
     <Box></Box>
    </Container>
