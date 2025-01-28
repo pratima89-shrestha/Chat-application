373dcfce94e6c2c67ccbea51af2ee7cbdf6ddfde
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-29 01:22:07 +0545

    mongoDb connection created in config of db.js

--- Server/server.js
+++ Server/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors'); // Import cors
 const dotenv = require('dotenv');
 const {chats} = require("./data/data");
+const connectDB = require('./config/db');
 
 
-
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
 app.use(cors());
 
