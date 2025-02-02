6e9dfbf361c0f59cb9b43510d266992ccfac0dfe
pratima89-shrestha <crexthapratima111@gmail.com>
2025-02-02 08:59:02 +0545

    error middle added for notfound and error handler

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const {chats} = require("./data/data");
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const {notFound,errorHandler}= require('./middleware/error')
 
 
 require('dotenv').config();
@@ -13,6 +14,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+//for errors of the user
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.use('/api/user',userRoutes); //path and middleware and the use is used to mount/attach the middleware
